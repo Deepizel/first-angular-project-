bc52154b80b21ffc62da5a8f0a5990ae04aa445d
deepizel <rocmancave@gmail.com>
2022-05-23 17:58:27 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import {Component} from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+   selector: 'app-root' ,
+   template: '<h1>{{pageTitle}}</h1>'
 })
-export class AppComponent {
-  title = 'pascodagama';
+
+export class AppComponent{
+   pageTitle: string = 'Acme Product Manager';
 }
